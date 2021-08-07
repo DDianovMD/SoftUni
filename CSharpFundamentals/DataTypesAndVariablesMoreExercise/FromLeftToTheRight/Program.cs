1260479fc4570efb33f41773b7da3ee6be645cbf
DDianovMD <ddianovmd@abv.bg>
2021-08-07 11:19:28 +0300

    Solved FromLeftToRight problem

--- CSharpFundamentals/DataTypesAndVariablesMoreExercise/FromLeftToTheRight/Program.cs
+++ CSharpFundamentals/DataTypesAndVariablesMoreExercise/FromLeftToTheRight/Program.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FromLeftToTheRight
 {
@@ -12,30 +7,192 @@ namespace FromLeftToTheRight
         static void Main(string[] args)
         {
             int numberOfInputs = int.Parse(Console.ReadLine());
-            BigInteger sum = 0;
+            int sum = 0;
             for (int i = 0; i < numberOfInputs; i++)
             {
                 string input = Console.ReadLine();
                 string leftNumber = input.Substring(0, input.IndexOf(" "));
                 string rightNumber = input.Substring(input.IndexOf(" ") + 1);
 
-                BigInteger firstNumbr = BigInteger.Parse(leftNumber);
-                BigInteger secondNumber = BigInteger.Parse(rightNumber);
+                long firstNumber = long.Parse(leftNumber);
+                long secondNumber = long.Parse(rightNumber);
 
-                if (firstNumbr > secondNumber)
+                if (firstNumber > secondNumber)
                 {
-                    sum += firstNumbr % 10;
-                    firstNumbr = firstNumbr / 10;
+                    if (leftNumber[0] != '-')
+                    {
+                        for (int j = 0; j < leftNumber.Length; j++)
+                        {
+                            sum += int.Parse(leftNumber[j].ToString());
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 1; j < leftNumber.Length; j++)
+                        {
+                            sum += int.Parse(leftNumber[j].ToString());
+                        }
+                    }
                 }
-                else
+                else if (firstNumber < secondNumber)
                 {
-                    sum += secondNumber % 10;
-                    firstNumbr = secondNumber / 10;
+                    if (rightNumber[0] != '-')
+                    {
+                        for (int j = 0; j < rightNumber.Length; j++)
+                        {
+                            sum += int.Parse(rightNumber[j].ToString());
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 1; j < rightNumber.Length; j++)
+                        {
+                            sum += int.Parse(rightNumber[j].ToString());
+                        }
+                    }
+                }
+                else if (firstNumber == secondNumber)
+                {
+                    if (leftNumber[0] != '-')
+                    {
+                        for (int j = 0; j < leftNumber.Length; j++)
+                        {
+                            sum += int.Parse(leftNumber[j].ToString());
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 1; j < leftNumber.Length; j++)
+                        {
+                            sum += int.Parse(leftNumber[j].ToString());
+                        }
+                    }
                 }
 
                 Console.WriteLine(sum);
                 sum = 0;
-            }   
+            }
+
+            // ANOTHER WORKING DECISION
+
+            //for (int i = 0; i < numberOfInputs; i++)
+            //{
+            //    string input = Console.ReadLine();
+            //    string leftNumber = input.Substring(0, input.IndexOf(" "));
+            //    string rightNumber = input.Substring(input.IndexOf(" ") + 1);
+            //    
+            //    if (leftNumber[0] != '-' && rightNumber[0] == '-') // check if first number is positive and second number is negative
+            //    {
+            //        for (int j = 0; j < leftNumber.Length; j++)
+            //        {
+            //            sum += int.Parse(leftNumber[j].ToString());
+            //        }
+            //    }
+            //    else if (leftNumber[0] == '-' && rightNumber[0] != '-') // check if first number is negative and second number is positive
+            //    {
+            //        for (int j = 0; j < rightNumber.Length; j++)
+            //        {
+            //            sum += int.Parse(rightNumber[j].ToString());
+            //        }
+            //    }
+            //    else if (leftNumber[0] != '-' && rightNumber[0] != '-') // check if both numbers are positive
+            //    {
+            //        if (leftNumber.Length > rightNumber.Length) // => first number is bigger
+            //        {
+            //            for (int j = 0; j < leftNumber.Length; j++)
+            //            {
+            //                sum += int.Parse(leftNumber[i].ToString());
+            //            }                        
+            //        }
+            //        else if (leftNumber.Length < rightNumber.Length) // => second number is bigger
+            //        {
+            //            for (int j = 0; j < rightNumber.Length; j++)
+            //            {
+            //                sum += int.Parse(rightNumber[j].ToString());
+            //            }
+            //        }
+            //        else if (leftNumber.Length == rightNumber.Length)
+            //        {
+            //            for (int j = 0; j < leftNumber.Length; j++)
+            //            {
+            //                if (leftNumber[j] > rightNumber[j]) // => first number is bigger
+            //                {
+            //                    for (int k = 0; k < leftNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(leftNumber[k].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //                else if (leftNumber[j] < rightNumber[j]) // => second number is bigger
+            //                {
+            //                    for (int k = 0; k < rightNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(rightNumber[k].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //                else if (leftNumber[j] == rightNumber[j] && j == leftNumber.Length - 1) // => both numbers are equal
+            //                {
+            //                    for (int k = 0; k < leftNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(leftNumber[k].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //            }
+            //        }
+            //    }
+            //    else if (leftNumber[0] == '-' && rightNumber[0] == '-') // check if both numbers are negative
+            //    {
+            //        if (leftNumber.Length < rightNumber.Length) // => first number is bigger
+            //        {
+            //            for (int j = 1; j < leftNumber.Length; j++)
+            //            {
+            //                sum += int.Parse(leftNumber[i].ToString());
+            //            }
+            //        }
+            //        else if (leftNumber.Length > rightNumber.Length) // => second number is bigger
+            //        {
+            //            for (int j = 1; j < rightNumber.Length; j++)
+            //            {
+            //                sum += int.Parse(rightNumber[j].ToString());
+            //            }
+            //        }
+            //        else if (leftNumber.Length == rightNumber.Length)
+            //        {
+            //            for (int j = 1; j < leftNumber.Length; j++)
+            //            {
+            //                if (leftNumber[j] < rightNumber[j]) // => left number is bigger
+            //                {
+            //                    for (int k = 1; k < leftNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(leftNumber[i].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //                else if (leftNumber[j] > rightNumber[j]) // => right number is bigger
+            //                {
+            //                    for (int k = 1; k < rightNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(rightNumber[k].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //                else if (leftNumber[j] == rightNumber[j] && j == leftNumber.Length - 1) // => both numbers are equal
+            //                {
+            //                    for (int k = 0; k < leftNumber.Length; k++)
+            //                    {
+            //                        sum += int.Parse(leftNumber[k].ToString());
+            //                    }
+            //                    break;
+            //                }
+            //            }
+            //        }
+            //    }
+            //
+            //    Console.WriteLine(sum);
+            //    sum = 0;
+            //}
         }
     }
 }
