f79c40a2cef0c518a271c972a4716bb3efce3ae0
DDianovMD <ddianovmd@abv.bg>
2021-08-07 11:38:58 +0300

    Solved FloatingEquality problem

---
+++ CSharpFundamentals/DataTypesAndVariablesMoreExercise/FloatingEquality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = double.Parse(Console.ReadLine());
+            double numberB = double.Parse(Console.ReadLine());
+
+            if (Math.Abs(numberA - numberB) < 0.000001)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
