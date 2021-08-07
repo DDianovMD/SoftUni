697f0e8aca20d98045137a6fd6f988e8c134b57f
DDianovMD <ddianovmd@abv.bg>
2021-08-07 13:37:05 +0300

    Solved BalancedBrackets problem

---
+++ CSharpFundamentals/DataTypesAndVariablesMoreExercise/BalancedBrackets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        public static int J { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int rowsNumber = int.Parse(Console.ReadLine());
+            int openingBracketsCounter = 0;
+            int closingBracketsCounter = 0;
+
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                string userInput = Console.ReadLine();
+                for (int j = 0; j < userInput.Length; j++)
+                {
+                    if (userInput[j] == '(' && openingBracketsCounter == closingBracketsCounter)
+                    {
+                        openingBracketsCounter++;
+                    }
+                    else if (userInput[j] == '(' && openingBracketsCounter > closingBracketsCounter)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        Environment.Exit(0);
+                    }
+                    else if (userInput[j] == ')')
+                    {
+                        closingBracketsCounter++;
+                    }
+                }
+            }
+
+            if (openingBracketsCounter == closingBracketsCounter)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
