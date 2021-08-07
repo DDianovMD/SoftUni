913cb6054024f729b98754276218f36438ed18db
DDianovMD <ddianovmd@abv.bg>
2021-08-07 12:41:32 +0300

    Solved DecryptingMessages problem

---
+++ CSharpFundamentals/DataTypesAndVariablesMoreExercise/DecryptingMessages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine()); // receiving user's key
+            int n = int.Parse(Console.ReadLine()); // number of lines, which will follow
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                char userInput = char.Parse(Console.ReadLine());
+                int asciiValue = key + (int)userInput;
+                decryptedMessage.Append(Convert.ToChar(asciiValue));
+            }
+
+            Console.WriteLine(decryptedMessage);
+        }
+    }
+}
