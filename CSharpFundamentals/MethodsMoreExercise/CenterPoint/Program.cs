6a66221e59b4363f7b3669ee5ffc165a11997943
DDianovMD <ddianovmd@abv.bg>
2021-08-14 18:42:36 +0300

    Solved CenterPoint problem

---
+++ CSharpFundamentals/MethodsMoreExercise/CenterPoint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x1 = decimal.Parse(Console.ReadLine());
+            decimal y1 = decimal.Parse(Console.ReadLine());
+            decimal x2 = decimal.Parse(Console.ReadLine());
+            decimal y2 = decimal.Parse(Console.ReadLine());
+
+            PrintClosestPointCoordinates(x1, y1, x2, y2);
+            
+        }
+
+        private static void PrintClosestPointCoordinates(decimal x1, decimal y1, decimal x2, decimal y2)
+        {
+            if (Math.Abs(x1) + Math.Abs(y1) < Math.Abs(x2) + Math.Abs(y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (Math.Abs(x1) + Math.Abs(y1) > Math.Abs(x2) + Math.Abs(y2))
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else if (Math.Abs(x1) + Math.Abs(y1) == Math.Abs(x2) + Math.Abs(y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+    }
+}
