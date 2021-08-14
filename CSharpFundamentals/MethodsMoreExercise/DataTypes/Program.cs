a03afb1cf556e4888b3eb35ebbd4e85e91f990ec
DDianovMD <ddianovmd@abv.bg>
2021-08-14 17:36:54 +0300

    Solved DataTypes problem

---
+++ CSharpFundamentals/MethodsMoreExercise/DataTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputType = Console.ReadLine();
+
+            if (userInputType.ToLower() == "int")
+            {
+                ReadInputAndPrintResult(int.Parse(Console.ReadLine()));
+            }
+            else if (userInputType.ToLower() == "real")
+            {
+                ReadInputAndPrintResult(double.Parse(Console.ReadLine()));
+            }
+            else if (userInputType.ToLower() == "string")
+            {
+                ReadInputAndPrintResult(Console.ReadLine());
+            }
+        }
+
+        public static void ReadInputAndPrintResult(string text)
+        {
+            Console.WriteLine($"${text}$");
+        }
+
+        public static void ReadInputAndPrintResult(double number)
+        {
+            Console.WriteLine($"{number * 1.5:F2}");
+        }
+
+        public static void ReadInputAndPrintResult(int number)
+        {
+            Console.WriteLine(number * 2);
+        }
+    }
+}
