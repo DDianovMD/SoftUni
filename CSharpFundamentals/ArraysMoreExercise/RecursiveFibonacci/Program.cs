eed67e90a76a404d8bef7e1519000a5ee5131487
DDianovMD <ddianovmd@abv.bg>
2021-08-14 14:11:00 +0300

    Solved RecursiveFibonacci problem

---
+++ CSharpFundamentals/ArraysMoreExercise/RecursiveFibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = int.Parse(Console.ReadLine());
+            int[] FibonacciSequence = new int[userInput];
+
+            for (int i = 0; i < FibonacciSequence.Length; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    FibonacciSequence[i] = 1;
+                }
+                else
+                {
+                    FibonacciSequence[i] = FibonacciSequence[i - 1] + FibonacciSequence[i - 2];
+                }
+            }
+
+            Console.WriteLine(FibonacciSequence[FibonacciSequence.Length - 1]);
+        }
+    }
+}
