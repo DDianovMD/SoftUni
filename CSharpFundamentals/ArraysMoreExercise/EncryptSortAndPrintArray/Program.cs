217ed1522b1e7b543e710625da2c7a2f1318cd28
DDianovMD <ddianovmd@abv.bg>
2021-08-14 12:42:34 +0300

    Solved Pascal's Triangle problem

---
+++ CSharpFundamentals/ArraysMoreExercise/EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = int.Parse(Console.ReadLine());
+            string[] names = new string[arrayLength];
+            int[] sortedNumbers = new int[names.Length];
+            // List<int> encryptedNames = new List<int>(names.Length);
+
+            int encryptedNumber = 0;
+            int savedSmallerNumber;
+            string currentName;                                 
+
+            for (int i = 0; i < names.Length; i++) // add names in the array
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < names.Length; i++) // calculate each name's encrypted number by given formula
+            {
+                currentName = names[i];
+                for (int j = 0; j < currentName.Length; j++)
+                {
+                    if (currentName[j] == 'a' || currentName[j] == 'e' || currentName[j] == 'i' || currentName[j] == 'o' || currentName[j] == 'u')
+                    {
+                        encryptedNumber += (int)currentName[j] * currentName.Length;
+                    }
+                    else
+                    {
+                        encryptedNumber += (int)currentName[j] / currentName.Length;
+                    }
+                }
+                // encryptedNames.Add(encryptedNumber); // adding number to list array
+                sortedNumbers[i] = encryptedNumber; // adding number to array
+                encryptedNumber = 0;
+            }
+
+            // encryptedNames.Sort();
+            // for (int i = 0; i < encryptedNames.Count; i++)
+            // {
+            //     Console.WriteLine(encryptedNames[i]);
+            // }
+
+            for (int i = 0; i < sortedNumbers.Length; i++) // sorting numbers in ascending order
+            {
+                for (int j = i + 1; j < sortedNumbers.Length; j++)
+                {
+                    if (sortedNumbers[i] > sortedNumbers[j])
+                    {
+                        savedSmallerNumber = sortedNumbers[j];
+                        sortedNumbers[j] = sortedNumbers[i];
+                        sortedNumbers[i] = savedSmallerNumber;
+                    }
+                }
+            }
+
+            for (int i = 0; i < sortedNumbers.Length; i++) // print result
+            {
+                Console.WriteLine(sortedNumbers[i]);
+            }
+                       
+        }
+    }
+}
---
+++ CSharpFundamentals/ArraysMoreExercise/PascalTriangle/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRows = int.Parse(Console.ReadLine());
+            int[] savedLastRow = new int[maxRows];
+
+            for (int row = 0; row < maxRows; row++)
+            {
+                int[] currentRow = new int[row + 1];
+
+                for (int i = 0; i < currentRow.Length; i++)
+                {
+                    if (row == 0)
+                    {
+                        currentRow[i] = 1;
+                    }
+                    else if (row == 1)
+                    {
+                        for (int j = 0; j <= row; j++)
+                        {
+                            currentRow[j] = 1;
+                        }
+                    }
+                    else if (row > 1 && row % 2 == 0)
+                    {
+                        for (int k = 0; k <= currentRow.Length / 2; k++) // fill first half of the row + middle item
+                        {
+                            if (k == 0)
+                            {
+                                currentRow[k] = 1;
+                            }
+                            else
+                            {
+                                currentRow[k] = savedLastRow[k - 1] + savedLastRow[k];
+                            }
+                        }
+                        int counter = 0;
+                        for (int l = currentRow.Length - 1; l > currentRow.Length / 2; l--) // fill second half of the row
+                        {
+                            currentRow[l] = currentRow[counter];
+                            counter++;
+                        }
+                    }
+                    else if (row > 1 && row % 2 == 1)
+                    {
+                        for (int m = 0; m < currentRow.Length / 2; m++) // fill first half of the row
+                        {
+                            if (m == 0)
+                            {
+                                currentRow[m] = 1;
+                            }
+                            else
+                            {
+                                currentRow[m] = savedLastRow[m - 1] + savedLastRow[m];
+                            }
+                        }
+                        int counter = 0;
+                        for (int n = currentRow.Length - 1; n >= currentRow.Length / 2; n--) // fill second half of the row
+                        {
+                            currentRow[n] = currentRow[counter];
+                            counter++;
+                        }
+
+                    }
+
+                    for (int z = 0; z < currentRow.Length; z++) // print current row of Pascal's triangle
+                    {
+                        if (z != currentRow.Length - 1)
+                        {
+                            Console.Write($"{currentRow[z]} ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(currentRow[z]);
+                        }
+                    }
+
+                    for (int f = 0; f < currentRow.Length; f++)
+                    {
+                        savedLastRow[f] = currentRow[f];
+                    }
+                    break;
+                }
+            }
+
+        }
+    }
+}
