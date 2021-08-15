4cf919f8ee32f71d952ef402a4d1d9a932e29ff3
DDianovMD <ddianovmd@abv.bg>
2021-08-15 11:43:08 +0300

    Solved AnonymousThreat problem by completing the code to work with command 'divide'

--- CSharpFundamentals/ListsExercise/AnonymousThreat/Program.cs
+++ CSharpFundamentals/ListsExercise/AnonymousThreat/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AnonymousThreat
 {
@@ -12,25 +11,30 @@ namespace AnonymousThreat
             List<string> userInput = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
+
             string[] command = ReceiveCommand();
 
-            string temporaryString = string.Empty;
+            string temporaryString = string.Empty; // used to form whole parts
 
-            while (command[0] != "3:1")
+            while (command[0] != "3:1") // looping while we receive command "3:1" which is the secret code for ending the cycle
             {
+                // code for command "merge"
+
                 if (command[0] == "merge")
                 {
                     int startIndex = int.Parse(command[1]);
                     int endIndex = int.Parse(command[2]);
 
+                    if (endIndex > userInput.Count)
+                    {
+                        endIndex = userInput.Count;
+                    }
+
                     if (startIndex < userInput.Count)
                     {
                         for (int i = startIndex; i < endIndex; i++)
-                        {
-                            if (i < userInput.Count)
-                            {
-                                temporaryString += userInput[i];
-                            }
+                        {                            
+                                temporaryString += userInput[i];                            
                         }
 
                         userInput.Insert(startIndex, temporaryString);
@@ -45,13 +49,96 @@ namespace AnonymousThreat
 
                 }
 
+                // code for command "divide"
+
+                else if (command[0] == "divide")
+                {
+                    int index = int.Parse(command[1]);
+                    int partitions = int.Parse(command[2]);
+                    string itemNeededToBeSplitted = userInput[index];
+                    int separatedPartsCount = 0; // counting how many parts are separated already
+
+                    List<char> charList = new List<char>(userInput[index].Length); // creating list of chars
+
+                    for (int i = 0; i < itemNeededToBeSplitted.Length; i++) // splitting item into single chars
+                    {
+                        charList.Add(itemNeededToBeSplitted[i]);
+                    }
+
+
+                    List<string> partsList = new List<string>(partitions); // creating list for parts
+
+                    if (charList.Count % partitions == 0) // if all parts are with equal length
+                    {                        
+                        for (int currentChar = 1; currentChar <= charList.Count; currentChar++) 
+                        {
+                            if (currentChar % (charList.Count / partitions) == 0) // check if we have all chars to form a whole part
+                            {                                
+                                for (int j = separatedPartsCount * (charList.Count / partitions); j < currentChar; j++) // grouping needed chars as whole part
+                                {
+                                    temporaryString += charList[j];
+                                }
+
+                                separatedPartsCount += 1;
+                                partsList.Add(temporaryString); // adding created part in the list
+                                temporaryString = string.Empty; // clearing temporary string used to create whole part
+                            }                            
+                        }
+
+                        for (int i = 0; i < partsList.Count; i++) // adding each part in the original list
+                        {
+                            userInput.Insert(index + 1 + i, partsList[i]);
+                        }
+
+                        userInput.RemoveAt(index); // removing splitted item from original list
+                        partsList.Clear(); // clearing partsList
+                    }
+                    else if (charList.Count % partitions != 0) // if all parts aren't with equal length
+                    {
+                        for (int currentChar = 1; currentChar <= charList.Count; currentChar++)
+                        {
+                            if (currentChar % (charList.Count / partitions) == 0 && currentChar != partitions + charList.Count % partitions) // check if we have all chars to form a whole part && we haven't yet reached the last part which is bigger than others
+                            {
+                                for (int j = separatedPartsCount * (charList.Count / partitions); j < currentChar; j++) // grouping needed chars as whole part
+                                {
+                                    temporaryString += charList[j];
+                                }
+
+                                separatedPartsCount += 1;
+                                partsList.Add(temporaryString); // adding created part in the list
+                                temporaryString = string.Empty; // clearing temporary string used to create whole part
+                            }
+                            else if (currentChar % (charList.Count / partitions) != 0 && currentChar == partitions + charList.Count % partitions) // check if we have all chars to form a whole part && we have reached the last part which is bigger than others
+                            {
+                                for (int j = separatedPartsCount * partitions; j <= charList.Count - 1; j++) // grouping needed chars as whole part (last part)
+                                {
+                                    temporaryString += charList[j];
+                                }
+
+                                separatedPartsCount += 1;
+                                partsList.Add(temporaryString); // adding created part in the list
+                                temporaryString = string.Empty; // clearing temporary string used to create whole part
+                                break; // breaking the cycle since we have formed the last part already
+                            }
+                        }
+
+                        for (int i = 0; i < partsList.Count; i++) // adding each part in the original list
+                        {
+                            userInput.Insert(index + 1 + i, partsList[i]);
+                        }
+
+                        userInput.RemoveAt(index); // removing splitted item from original list
+                        partsList.Clear(); // clearing partsList
+                    }
+                }
+
                 command = ReceiveCommand();
             }
 
             Console.WriteLine(string.Join(" ", userInput));
         }
 
-        private static string[] ReceiveCommand()
+        public static string[] ReceiveCommand()
         {
             return Console.ReadLine().ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
         }
---
+++ CSharpFundamentals/MethodsMoreExercise/LongerLine/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace LongerLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            double lengthFirstLine = lineLength(x1, y1, x2, y2);
+            double lengthSecondLine = lineLength(x3, y3, x4, y4);
+
+            if (lengthFirstLine >= lengthSecondLine)
+            {
+                bool isFirstCloser = closerPoint(x1, y1, x2, y2);
+                if (isFirstCloser)
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+            }
+            else if (lengthFirstLine < lengthSecondLine)
+            {
+                bool isFirstCloser = closerPoint(x3, y3, x4, y4);
+                if (isFirstCloser)
+                {
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+                }
+            }
+        }
+
+        private static double lineLength(double x1, double y1, double x2, double y2)
+        {
+            double a1 = Math.Abs(x1) + Math.Abs(x2);
+            double b1 = Math.Abs(Math.Abs(y1) - Math.Abs(y2));
+            return Math.Sqrt(a1 * a1 + b1 * b1);
+        }
+
+        private static bool closerPoint(double x1, double y1, double x2, double y2)
+        {
+            double c1 = Math.Sqrt(x1 * x1 + y1 * y1);
+            double c2 = Math.Sqrt(x2 * x2 + y2 * y2);
+            bool closerOrNot = true;
+            if (c1 < c2)
+            {
+                closerOrNot = true;
+            }
+            else if (c1 > c2)
+            {
+                closerOrNot = false;
+            }
+            return closerOrNot;
+        }
+    }
+}
