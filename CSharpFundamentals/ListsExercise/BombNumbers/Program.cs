7e0227d70b28b026db84c4df8c9c70aca7f61f7b
DDianovMD <ddianovmd@abv.bg>
2021-10-03 10:35:11 +0300

    Fixed errors on tests in Judge for BombNumbers problem

--- CSharpFundamentals/ListsExercise/BombNumbers/Program.cs
+++ CSharpFundamentals/ListsExercise/BombNumbers/Program.cs
@@ -16,24 +16,27 @@ namespace BombNumbers
             int[] bombNumberAndPower = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
-                .ToArray();            
-           
+                .ToArray();
+
             int startIndex;
-            int times = bombNumberAndPower[1] * 2 + 1;
-            int sum = 0;            
-            
+            int sum = 0;
+
             while (listOfIntegers.Contains(bombNumberAndPower[0]))
-            {                
+            {
+
                 startIndex = listOfIntegers.IndexOf(bombNumberAndPower[0]) - bombNumberAndPower[1];
 
-                for (int i = 0; i < times; i++)
+                if (startIndex < 0)             //тук
+                    startIndex = 0;
+
+                for (int i = 0; i < bombNumberAndPower[1] * 2 + 1; i++)
                 {
                     if (startIndex <= listOfIntegers.Count - 1)
                     {
                         listOfIntegers.RemoveAt(startIndex);
-                    }                    
+                    }
                 }
-                
+
             }
 
             for (int i = 0; i < listOfIntegers.Count; i++)
