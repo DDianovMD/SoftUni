aba7acb6bcc796162250c324e45ad1b05317d3bf
DDianovMD <ddianovmd@abv.bg>
2021-10-03 20:03:32 +0300

    Solved Courses problem

---
+++ CSharpFundamentals/AssociativeArraysExercise/Courses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue = true;
+
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (isTrue)
+            {
+                string[] command = Console.ReadLine()
+                   .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+                   .ToArray();
+
+                if (command[0].ToLower() == "end")
+                {
+                    isTrue = false;
+                    break;
+                }
+                else
+                {
+                    if (courses.ContainsKey(command[0]) == false)
+                    {
+                        courses.Add(command[0], new List<string> { command[1] });
+                    }
+                    else
+                    {
+                        courses[command[0]].Add(command[1]);
+                    }
+                }
+            }
+            
+            foreach (var course in courses.OrderByDescending(n => n.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+                List<string> usersList = course.Value.ToList();
+                usersList.Sort();
+                
+                for (int i = 0; i < course.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {usersList[i]}");
+                }
+            }
+        }
+    }
+}
