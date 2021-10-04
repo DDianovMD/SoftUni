c27c3280a8a387243aed436162ddb5e930056b45
DDianovMD <ddianovmd@abv.bg>
2021-10-04 18:52:31 +0300

    Solved ExtractFile problem

---
+++ CSharpFundamentals/StringAndTextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userInput = Console.ReadLine()
+                .Split("\\", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string fileName = userInput[userInput.Length - 1];
+
+            string[] splittedFile = fileName
+                .Split(".", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Console.WriteLine($"File name: {splittedFile[0]}");
+            Console.WriteLine($"File extension: {splittedFile[1]}");
+        }
+    }
+}
