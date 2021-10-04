3907489c1d8b0d0865a3fc41c28bfd0e5d1b18dc
DDianovMD <ddianovmd@abv.bg>
2021-10-04 18:41:43 +0300

    Solved CharacterMultiplier problem

---
+++ CSharpFundamentals/StringAndTextProcessing/CharacterMultiplier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+
+            int result = CharacterMultiplier(strings[0], strings[1]);
+            Console.WriteLine(result);
+
+        }
+
+        private static int CharacterMultiplier(string firstString, string secondString)
+        {
+            int sum = 0;
+            string shorterString = string.Empty;
+            string longerString = string.Empty;
+
+            if (firstString.Length < secondString.Length)
+            {
+                shorterString = firstString;
+                longerString = secondString;
+            }
+            else if (firstString.Length >= secondString.Length)
+            {
+                shorterString = secondString;
+                longerString = firstString;
+            }
+
+            for (int i = 0; i < shorterString.Length; i++)
+            {
+                sum += (int)shorterString[i] * (int)longerString[i];
+            }
+
+            for (int i = shorterString.Length; i < longerString.Length; i++)
+            {
+                sum += longerString[i];
+            }
+
+            return sum;
+        }
+    }
+}
