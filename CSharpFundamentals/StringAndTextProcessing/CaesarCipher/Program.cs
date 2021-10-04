a1450e49fb7a68215b58824f845e66fde71839d7
DDianovMD <ddianovmd@abv.bg>
2021-10-04 19:07:24 +0300

    Solved CaesarCipher problem

---
+++ CSharpFundamentals/StringAndTextProcessing/CaesarCipher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+
+            StringBuilder encryptedInput = new StringBuilder();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                int charASCIIcode = (int)userInput[i];
+
+                int charEncrypting = charASCIIcode + 3;
+                
+                encryptedInput.Append(((char)charEncrypting).ToString());
+            }
+
+            Console.WriteLine(encryptedInput);
+        }
+    }
+}
