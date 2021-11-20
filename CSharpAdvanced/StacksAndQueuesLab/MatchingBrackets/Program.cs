a50d4eecea9489445a9e78ad62a4695c7a1342de
DDianovMD <ddianovmd@abv.bg>
2021-11-20 20:04:40 +0200

    Solved MatchingBrackets problem

---
+++ CSharpAdvanced/StacksAndQueuesLab/MatchingBrackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] userInput = Console.ReadLine().ToCharArray();
+
+            Stack<int> openingBracketsIndexes = new Stack<int>();
+            Queue<int> closingBracketsIndexes = new Queue<int>();
+
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == '(')
+                {
+                    openingBracketsIndexes.Push(i);
+                }
+                else if (userInput[i] == ')')
+                {
+                    for (int j = openingBracketsIndexes.Peek(); j <= i; j++)
+                    {
+                        Console.Write(userInput[j]);
+                    }
+                    Console.WriteLine();
+                    openingBracketsIndexes.Pop();
+                }
+            }
+        }
+    }
+}
