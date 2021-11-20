9c7246fb9623702b817359fe4aaf920e4b580a62
DDianovMD <ddianovmd@abv.bg>
2021-11-20 22:19:04 +0200

    Solved PrintEvenNumbers problem

---
+++ CSharpAdvanced/StacksAndQueuesLab/PrintEvenNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> intQueue = new Queue<int>();
+
+            foreach (var number in userInput)
+            {
+                intQueue.Enqueue(number);
+            }
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                
+
+                if (intQueue.Peek() % 2 == 0 && i < userInput.Length - 1)
+                {
+                    Console.Write($"{intQueue.Peek()}, ");                    
+                }
+                else if (intQueue.Peek() % 2 == 0 && i == userInput.Length - 1)
+                {
+                    Console.WriteLine(intQueue.Peek());                    
+                }
+                intQueue.Dequeue();
+            }
+        }
+    }
+}
