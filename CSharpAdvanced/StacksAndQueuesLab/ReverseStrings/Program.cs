b8f1f46a1b35d241ae383baed8a56540abf1a2a6
DDianovMD <ddianovmd@abv.bg>
2021-11-20 17:54:45 +0200

    Solved ReverseStrings problem

---
+++ CSharpAdvanced/StacksAndQueuesLab/ReverseStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            Stack<char> charStack = new Stack<char>();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {                
+                charStack.Push(userInput[i]);
+            }
+
+            while (charStack.Count != 0)
+            {
+                Console.Write(charStack.Peek());
+                charStack.Pop();
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsFinalExams/09.08.2020/PlantDiscovery/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlantDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> plantsRarity = new Dictionary<string, int>();
+            Dictionary<string, List<int>> plantsRatings = new Dictionary<string, List<int>>();
+
+            for (int i = 0; i < times; i++)
+            {
+                string[] userInput = Console.ReadLine()
+                    .Split("<->", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (plantsRarity.ContainsKey(userInput[0]) == false)
+                {
+                    plantsRarity.Add(userInput[0], int.Parse(userInput[1]));
+                    plantsRatings.Add(userInput[0], new List<int>());
+                }
+                else
+                {
+                    plantsRarity[userInput[0]] = int.Parse(userInput[1]);
+                }
+
+            }
+
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            while (command[0] != "Exhibition")
+            {
+
+                if (command[0] == "Rate:")
+                {
+                    plantsRatings[command[1]].Add(int.Parse(command[3]));
+                }
+                else if (command[0] == "Update:" && plantsRarity[command[1]] != int.Parse(command[3]))
+                {
+                    plantsRarity[command[1]] = int.Parse(command[3]);
+                }
+                else if (command[0] == "Reset:")
+                {
+                    plantsRatings[command[1]].Clear();
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+                        
+                    command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+            }
+
+            foreach (var plant in plantsRatings)
+            {
+                if (plant.Value.Count() == 0)
+                {
+                    plant.Value.Add(0);
+                }
+            }
+
+            // output
+
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var plant in plantsRarity.OrderByDescending(rarity => rarity.Value).ThenByDescending(w => plantsRatings[w.Key].Average()))
+            {
+                Console.WriteLine($"- {plant.Key}; Rarity: {plant.Value}; Rating: {plantsRatings[plant.Key].Average():F2}");
+            }
+
+        }
+    }
+}
