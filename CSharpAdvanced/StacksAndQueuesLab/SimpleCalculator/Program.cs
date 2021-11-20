f2db991160fb15746f7683f624f636671cfcd2cd
DDianovMD <ddianovmd@abv.bg>
2021-11-20 18:56:39 +0200

    Solved SimpleCalculator problem

---
+++ CSharpAdvanced/StacksAndQueuesLab/SimpleCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] equation = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Stack<int> result = new Stack<int>();
+
+            for (int i = 0; i < int.Parse(equation[0]); i++)
+            {
+                result.Push(1);
+            }
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (equation[i] == "+")
+                {
+                    for (int j = 0; j < int.Parse(equation[i + 1]); j++)
+                    {
+                        result.Push(1);
+                    }
+                }
+                else if (equation[i] == "-")
+                {
+                    for (int k = 0; k < int.Parse(equation[i + 1]); k++)
+                    {
+                        result.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine(result.Count());
+        }
+    }
+}
