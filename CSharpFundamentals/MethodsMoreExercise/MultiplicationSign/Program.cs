bb3d4f71bbf064069547a0470ed8bfdfb10e14f0
DDianovMD <ddianovmd@abv.bg>
2021-12-18 21:32:29 +0200

    Solved MultiplicationSign problem

---
+++ CSharpFundamentals/MethodsMoreExercise/MultiplicationSign/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MultiplicationSign
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] givenNumbers = new int[3];
+
+			for (int i = 0; i < givenNumbers.Length; i++)
+			{
+				givenNumbers[i] = int.Parse(Console.ReadLine());
+				if (givenNumbers[i] == 0)
+				{
+					Console.WriteLine("zero");
+					Environment.Exit(0);
+				}
+			}
+
+			int negativeNumbersCount = 0;
+
+			foreach (var num in givenNumbers)
+			{
+				if (num < 0)
+				{
+					negativeNumbersCount++;
+				}
+			}
+
+			if (negativeNumbersCount % 2 == 0 || negativeNumbersCount == 0)
+			{
+				Console.WriteLine("positive");
+			}
+			else
+			{
+				Console.WriteLine("negative");
+			}
+
+		}
+	}
+}
