cf856c6329f3ca64150cacdf5915d33021e20343
DDianovMD <ddianovmd@abv.bg>
2021-12-19 12:42:31 +0200

    Solved CarRace problem

---
+++ CSharpFundamentals/ListsMoreExercise/CarRace/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] racingTrace = Console.ReadLine() // One Judge test fails when calculating with decimal instead double.
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            double leftRacerTime = 0;
+            double rightRacerTime = 0;
+
+            // Calculating left racer time
+            for (int i = 0; i < racingTrace.Length / 2; i++)
+            {
+                if (racingTrace[i] == 0)
+                {
+                    leftRacerTime *= 0.8;
+                }
+                else
+                {
+                    leftRacerTime += racingTrace[i];
+                }
+            }
+
+            // Calculating right racer time
+            for (int i = racingTrace.Length - 1; i > racingTrace.Length / 2; i--)
+            {
+                if (racingTrace[i] == 0)
+                {
+                    rightRacerTime *= 0.8;
+                }
+                else
+                {
+                    rightRacerTime += racingTrace[i];
+                }
+            }
+
+            // Print winner            
+            if (leftRacerTime < rightRacerTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftRacerTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightRacerTime}");
+            }            
+        }
+    }
+}
