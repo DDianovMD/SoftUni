1879b57972836c5e34fc90fef3e6dd23b65f0fa0
DDianovMD <ddianovmd@abv.bg>
2021-12-19 11:57:35 +0200

    Solved Messaging problem

---
+++ CSharpFundamentals/ListsMoreExercise/Messaging/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<char> secretMessage = Console.ReadLine()
+                .ToList();
+
+            StringBuilder decodedMessage = new StringBuilder();
+
+            foreach (var number in numbers)
+            {
+                int sum = 0;
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    sum += int.Parse(number[i].ToString());
+                }
+
+                if (sum <= secretMessage.Count)
+                {
+                    decodedMessage.Append(secretMessage[sum]);
+                    secretMessage.RemoveAt(sum);
+                }
+                else
+                {
+                    while (sum > secretMessage.Count)
+                    {
+                        sum -= secretMessage.Count;
+                    }
+
+                    decodedMessage.Append(secretMessage[sum]);
+                    secretMessage.RemoveAt(sum);
+                }
+            }
+
+            Console.WriteLine(decodedMessage);
+
+        }
+    }
+}
