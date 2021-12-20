7acd135c8fe51a303df684645c015b13f8b1688a
DDianovMD <ddianovmd@abv.bg>
2021-12-20 14:40:08 +0200

    Solved BigFactorial problem

---
+++ CSharpFundamentals/ObjectsАndClassesLab/BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger sum = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                sum *= i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
