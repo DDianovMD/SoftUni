693d42de66350d7c50ba158a5e810603c2599f2f
DDianovMD <ddianovmd@abv.bg>
2021-12-20 10:25:59 +0200

    Solved Oscars problem

---
+++ CSharpBasics/ForLoopExercise/Oscars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double score = double.Parse(Console.ReadLine());
+
+            int judges = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < judges; i++)
+            {
+                string currentJudge = Console.ReadLine();
+                double judgeGrade = double.Parse(Console.ReadLine());
+
+                score += currentJudge.Length * judgeGrade / 2.0;
+
+                if (score > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {score:F1}!");
+                    break;
+                }
+
+                if (i == judges - 1 && score < 1250.5)
+                {
+                    Console.WriteLine($"Sorry, {actorName} you need {1250.5 - score:F1} more!");
+                }
+            }
+        }
+    }
+}
