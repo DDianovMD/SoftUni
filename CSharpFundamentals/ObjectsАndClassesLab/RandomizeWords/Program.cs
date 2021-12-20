de909b9fc39b61d806d8658f6c933cbf59f0fd09
DDianovMD <ddianovmd@abv.bg>
2021-12-20 14:20:50 +0200

    Solved RandomizeWords problem

---
+++ CSharpFundamentals/ObjectsАndClassesLab/RandomizeWords/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word1 = words[i];
+                string word2 = words[rnd.Next(0, words.Count - 1)];
+
+                words.Insert(i, word2);
+                words.Insert(words.LastIndexOf(word2), word1);
+                words.RemoveAt(words.LastIndexOf(word2));
+                words.RemoveAt(i + 1);
+
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
