01a7f0663135627d58f45a1b51b300562467e311
DDianovMD <ddianovmd@abv.bg>
2021-12-24 14:49:20 +0200

    Solved Students problem

---
+++ CSharpFundamentals/ObjectsAndClassesExercise/Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Students
+{
+	public class Student
+	{
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public double grade { get; set; }
+
+		public Student(string firstName, string lastName, double grade)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.grade = grade;
+		}
+
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			int studentsCount = int.Parse(Console.ReadLine());
+
+			List<Student> studentsList = new List<Student>();
+
+			for (int i = 0; i < studentsCount; i++)
+			{
+				string[] userInput = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+					.ToArray();
+
+				Student currentStudent = new Student(userInput[0], userInput[1], double.Parse(userInput[2]));
+
+				studentsList.Add(currentStudent);
+			}
+
+			foreach (var student in studentsList.OrderByDescending(x => x.grade))
+			{
+				Console.WriteLine($"{student.firstName} {student.lastName}: {student.grade:F2}");
+			}
+		}
+    }
+}
