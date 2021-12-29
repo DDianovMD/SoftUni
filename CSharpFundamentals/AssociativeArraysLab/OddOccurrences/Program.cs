0e90fa60968f5ee953789008cf36f0f4c3baecc4
DDianovMD <ddianovmd@abv.bg>
2021-12-29 12:59:11 +0200

    Solved OddOccurrences problem

---
+++ CSharpFundamentals/AssociativeArraysLab/OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .ToLower()
+                .Split(" ");
+
+            Dictionary<string, int> wordOccurence = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordOccurence.ContainsKey(word) == false)
+                {
+                    wordOccurence.Add(word, 1);
+                }
+                else
+                {
+                    wordOccurence[word]++;
+                }
+            }
+
+            foreach (var word in wordOccurence)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
