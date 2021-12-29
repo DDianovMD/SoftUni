7a3fef47e01acb4a5cc0c74adc02cf182c19dbbc
DDianovMD <ddianovmd@abv.bg>
2021-12-29 15:23:48 +0200

    Solved WordFilter problem

---
+++ CSharpFundamentals/AssociativeArraysLab/WordFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filteredWords = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length % 2 == 0)
+                .ToArray();
+
+            foreach (var word in filteredWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
