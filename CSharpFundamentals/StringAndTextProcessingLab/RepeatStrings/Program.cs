88d10195b32ddb2e398c86712b248fb7a9a58bcc
DDianovMD <ddianovmd@abv.bg>
2022-01-04 12:46:06 +0200

    Solved RepeatStrings problem

---
+++ CSharpFundamentals/StringAndTextProcessingLab/RepeatStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string tempString = string.Empty;
+
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    tempString += words[i];
+                }
+                words[i] = tempString;
+            }
+
+            Console.WriteLine(string.Join("", words));
+        }
+    }
+}
