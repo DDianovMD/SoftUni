ed64ed4c611e3012fa58c51808ff35fac01107eb
DDianovMD <ddianovmd@abv.bg>
2022-01-04 12:37:47 +0200

    Solved ReverseStrings problem

---
+++ CSharpFundamentals/StringAndTextProcessingLab/ReverseStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                words.Add(command);
+
+                command = Console.ReadLine();
+            }
+
+            List<string> reversedString = new List<string>();
+            string reversedWord = string.Empty;
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                for (int j = words[i].Length - 1; j >= 0 ; j--)
+                {
+                    reversedWord += words[i][j];
+                }
+                reversedString.Add(reversedWord);
+                reversedWord = string.Empty;
+            }
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                Console.WriteLine($"{words[i]} = {reversedString[i]}");
+            }
+        }
+    }
+}
