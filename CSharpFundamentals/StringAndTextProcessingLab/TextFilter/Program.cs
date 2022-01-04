5d63a36c1e67cc46212eb43ad4c1851b84d9592a
DDianovMD <ddianovmd@abv.bg>
2022-01-04 13:56:05 +0200

    Solved TextFilter problem

---
+++ CSharpFundamentals/StringAndTextProcessingLab/TextFilter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bannedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                string replaceString = string.Empty;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    replaceString += "*";
+                }
+
+                while (text.IndexOf(word) != -1)
+                {
+                    text = text.Replace(word, replaceString);
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
