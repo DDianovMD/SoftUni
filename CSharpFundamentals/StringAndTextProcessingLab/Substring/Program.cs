32d6288a89a5580edf0901efcfaf2bb382f9ffc5
DDianovMD <ddianovmd@abv.bg>
2022-01-04 13:27:06 +0200

    Solved Substring problem

---
+++ CSharpFundamentals/StringAndTextProcessingLab/Substring/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToBeRemoved = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            
+            while (true)
+            {
+                if (text.IndexOf(textToBeRemoved) != -1)
+                {
+                    text = text.Remove(text.IndexOf($"{textToBeRemoved}"), textToBeRemoved.Length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{text}");
+        }
+    }
+}
