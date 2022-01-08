7fde5ffd232f7cc01c602ca94344b6b746c3ae36
DDianovMD <ddianovmd@abv.bg>
2022-01-08 16:49:16 +0200

    Solved SuppliesForSchool problem

---
+++ CSharpBasics/FirstStepsInCodingExercise/SuppliesForSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SuppliesForSchool
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pensCount = int.Parse(Console.ReadLine());
+            int markersCount = int.Parse(Console.ReadLine());
+            int sanitizerVolume = int.Parse(Console.ReadLine());
+            int percentsDiscount = int.Parse(Console.ReadLine());
+
+            double price = pensCount * 5.80 + markersCount * 7.20 + sanitizerVolume * 1.20;
+
+            Console.WriteLine(price - (price * percentsDiscount / 100));
+
+
+        }
+    }
+}
