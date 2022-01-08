4f196fbd09054ab76226fa3c985eb0ad1204e9c0
DDianovMD <ddianovmd@abv.bg>
2022-01-09 01:37:45 +0200

    Solved ASCIISumator problem

---
+++ CSharpFundamentals/StringAndTextProcessingMoreExercise/ASCIISumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ASCIISumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ASCIIStartIndex = (int)char.Parse(Console.ReadLine());
+            int ASCIIEndIndex = (int)char.Parse(Console.ReadLine());
+
+            string userInput = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if ((int)userInput[i] > ASCIIStartIndex && (int)userInput[i] < ASCIIEndIndex)
+                {
+                    sum += (int)userInput[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
