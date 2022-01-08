38ba80becc89006ec6cc9cfb12826fdf45fc43ab
DDianovMD <ddianovmd@abv.bg>
2022-01-08 17:38:55 +0200

    Solved BasketballEquipment problem

---
+++ CSharpBasics/FirstStepsInCodingExercise/BasketballEquipment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasketballEquipment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int priceForOneYear = int.Parse(Console.ReadLine());
+
+            double trainersPrice = priceForOneYear - (priceForOneYear * 0.4);
+            double outfitPrice = trainersPrice - (trainersPrice * 0.2);
+            double ballPrice = outfitPrice * 0.25;
+            double accessoriesPrice = ballPrice * 0.2;
+
+            double moneyNeeded = priceForOneYear + trainersPrice + outfitPrice + ballPrice + accessoriesPrice;
+
+            Console.WriteLine(moneyNeeded);
+        }
+    }
+}
