ffbfc0b9b30ac1549d35577f6c0212178f9c02e5
DDianovMD <ddianovmd@abv.bg>
2022-01-08 17:08:26 +0200

    Solved Repainting problem

---
+++ CSharpBasics/FirstStepsInCodingExercise/Repainting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nylon = int.Parse(Console.ReadLine()) + 2;
+            double paintVolume = double.Parse(Console.ReadLine()) * 1.1;
+            int diluentVolume = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double materialsPrice = nylon * 1.5 + paintVolume * 14.5 + diluentVolume * 5 + 0.4;
+
+            double moneyForHour = materialsPrice * 0.30;
+
+            double neededMoney = materialsPrice + moneyForHour * hours;
+
+            Console.WriteLine(neededMoney);            
+        }
+    }
+}
