e657802ae64ee44c9d0641025b7adeaf1e570183
DDianovMD <ddianovmd@abv.bg>
2022-01-08 17:23:00 +0200

    Solved FoodDelivery problem

---
+++ CSharpBasics/FirstStepsInCodingExercise/FoodDelivery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main()
+        {
+            int chickenMenusCount = int.Parse(Console.ReadLine());
+            int fishMenusCount = int.Parse(Console.ReadLine());
+            int vegetarianMenusCount = int.Parse(Console.ReadLine());
+
+            double checkenMenuPrice = 10.35;
+            double fishMenuPrice = 12.4;
+            double vegetarianMenuPrice = 8.15;
+
+            double desertPrice = (chickenMenusCount * checkenMenuPrice + fishMenusCount * fishMenuPrice + vegetarianMenusCount * vegetarianMenuPrice) * 0.2;
+
+            double moneyNeeded = chickenMenusCount * checkenMenuPrice + fishMenusCount * fishMenuPrice + vegetarianMenusCount * vegetarianMenuPrice + desertPrice + 2.5;
+
+            Console.WriteLine(moneyNeeded);
+        }
+    }
+}
