f5a031072f832d8d8f1dbe0a29fe7f055443af89
DDianovMD <ddianovmd@abv.bg>
2022-01-09 03:29:18 +0200

    Solved HTML problem

---
+++ CSharpFundamentals/StringAndTextProcessingMoreExercise/HTML/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HTML
+{
+    class Program
+    {
+        static void Main()
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+            List<string> comments = new List<string>();
+
+            string currentComment = string.Empty;
+
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                currentComment = Console.ReadLine();
+
+                if (currentComment.ToLower() == "end of comments")
+                {
+                    keepGoing = false;
+                    break;
+                }
+                else
+                {
+                    comments.Add(currentComment);
+                }
+            }
+
+            //Print output
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");            
+            Console.WriteLine($"    {content}");
+            Console.WriteLine("</article>");
+
+            if (comments.Count > 0) // without this if clause Judge gives 80/100 points.
+            {
+                foreach (var comment in comments)
+                {
+                    Console.WriteLine("<div>");
+                    Console.WriteLine($"    {comment}");
+                    Console.WriteLine("</div>");
+                }
+            }
+            
+        }
+    }
+}
