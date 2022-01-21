71128df55997d9e849de29d3d3d638ef75a7f8e3
DDianovMD <ddianovmd@abv.bg>
2022-01-21 21:45:44 +0200

    Solved MatchPhoneNumber problem

---
+++ CSharpFundamentals/RegularExpressionsLab/MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumberPattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            Regex regex = new Regex(phoneNumberPattern);
+
+            string phones = Console.ReadLine();
+
+            Console.WriteLine(string.Join(", ", regex.Matches(phones)));
+        }
+    }
+}
