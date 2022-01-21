6234ea781f9c6f8ee36dbeb44cfc4d087584ca58
DDianovMD <ddianovmd@abv.bg>
2022-01-21 21:12:01 +0200

    Solved MatchFullName problem

---
+++ CSharpFundamentals/RegularExpressionsLab/MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullNamePattern = @"\b[A-Z][a-z]+ \b[A-Z][a-z]+";
+
+            Regex regex = new Regex(fullNamePattern);
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(string.Join(" ", regex.Matches(userInput)));
+        }
+    }
+}
