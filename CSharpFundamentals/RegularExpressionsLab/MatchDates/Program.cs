da1ec6215911a9a9f5504f2f169d3d96bd42eb5e
DDianovMD <ddianovmd@abv.bg>
2022-01-25 16:07:21 +0200

    Solved MatchDates problem

---
+++ CSharpFundamentals/RegularExpressionsLab/MatchDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datesPattern = @"\b(?<day>0[1-9]|[1-2][0-9]|3[0-2])(\.|\-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            string userInput = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(userInput, datesPattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"].Value}, Month: {match.Groups["month"].Value}, Year: {match.Groups["year"].Value}");
+            }
+        }
+    }
+}
