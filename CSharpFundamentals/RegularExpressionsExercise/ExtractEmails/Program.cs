cda2531c01594615a422b4c51dc09ef2faf2e706
DDianovMD <ddianovmd@abv.bg>
2022-01-29 13:32:57 +0200

    Solved ExtractEmails problem

---
+++ CSharpFundamentals/RegularExpressionsExercise/ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string emailPattern = @"[\w\-_\.]+@[a-zA-Z\-]+\.[a-zA-Z.\-]+\b";
+
+            MatchCollection emails = Regex.Matches(input, emailPattern);
+
+            foreach (var email in emails)
+            {
+                if (email.ToString()[0] != '.' && email.ToString()[0] != '-' && email.ToString()[0] != '_')
+                {
+                    Console.WriteLine(email.ToString());
+                }
+            }
+        }
+    }
+}
