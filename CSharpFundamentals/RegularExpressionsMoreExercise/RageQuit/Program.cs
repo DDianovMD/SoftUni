eb33d6c38c4d4df7128bd4be1bb38ea9703e5707
DDianovMD <ddianovmd@abv.bg>
2022-02-05 13:27:54 +0200

    Solved RageQuit problem

---
+++ CSharpFundamentals/RegularExpressionsMoreExercise/RageQuit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine().ToUpper();
+            string numberPattern = @"\d+";
+            string wordPattern = @"\D+";
+
+            var words = Regex.Matches(userInput, wordPattern);
+            var numbers = Regex.Matches(userInput, numberPattern);
+
+            StringBuilder rageString = new StringBuilder();  
+            
+            for (int i = 0; i < words.Count; i++)
+            {
+                for (int j = int.Parse(numbers[i].ToString()); j > 0; j--)
+                {
+                    rageString.Append(words[i]);
+                }
+            }
+
+            int uniqueSymbolsCount = rageString.ToString().Distinct().Count();
+
+            // Print output
+            Console.WriteLine($"Unique symbols used: {uniqueSymbolsCount}");
+            Console.WriteLine(rageString);
+        }
+    }
+}
