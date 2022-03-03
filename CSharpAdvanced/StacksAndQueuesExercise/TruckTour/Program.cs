286eb70a93b12489999cf1a0128027e41b61eba7
DDianovMD <ddianovmd@abv.bg>
2022-03-03 13:46:25 +0200

    Solved TruckTour problem

--- CSharpAdvanced/StacksAndQueuesExercise/TruckTour/Program.cs
+++ CSharpAdvanced/StacksAndQueuesExercise/TruckTour/Program.cs
@@ -6,16 +6,13 @@ namespace TruckTour
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int pupmsCount = int.Parse(Console.ReadLine());
-            Queue<int> petrolAmount = new Queue<int>();
-            Queue<int> distance = new Queue<int>();
-            int startIndex = 0;
-            int checkpointCounter = 0;
+            int pupmsCount = int.Parse(Console.ReadLine()); // == N
+            Queue<int> petrolAmount = new Queue<int>(); // amount of petrol that current pump gives
+            Queue<int> distance = new Queue<int>(); // distance toward next pump
 
-            int fuel = 0;
-
+            // Fill queues with input numbers
             for (int i = 0; i < pupmsCount; i++)
             {
                 int[] userInput = Console.ReadLine()
@@ -24,39 +21,44 @@ namespace TruckTour
                     .ToArray();
 
                 petrolAmount.Enqueue(userInput[0]);
-                distance.Enqueue(userInput[1]);                
+                distance.Enqueue(userInput[1]);
             }
 
-            
-                
-            while (checkpointCounter < pupmsCount)
-            {                
+            int fuel = petrolAmount.Peek(); // set initial fuel from first station
+            int smallestIndex = 0; // first station index
+            int currentIndex = 0; // current index
+            int stationsPassedCounter = 0; // counter
 
-                fuel += petrolAmount.Peek();
+            while (stationsPassedCounter <= pupmsCount) // cycling while we pass through all stations
+            {
+                if (stationsPassedCounter == 0)
+                {
+                    smallestIndex = currentIndex; // set starting index
+                }
 
-                if (fuel >= distance.Peek())
-                {
-                    fuel -= distance.Peek();
-                    petrolAmount.Enqueue(petrolAmount.Peek());
-                    petrolAmount.Dequeue();
-                    distance.Enqueue(distance.Peek());
-                    distance.Dequeue();
-                                        
-                    checkpointCounter++;
+                if (fuel >= distance.Peek()) // if we have enough amount of fuel to reach next station
+                {                    
+                    fuel -= distance.Peek(); // go to next station (1 L fuel == 1 km distance)
+                    stationsPassedCounter++; // +1 passed station
+
+                    petrolAmount.Enqueue(petrolAmount.Dequeue()); // current station is put last in the queue
+                    distance.Enqueue(distance.Dequeue());                    
+                    fuel += petrolAmount.Peek(); // load fuel from next station
                 }
-                else
-                {
-                    fuel = 0;
-                    petrolAmount.Enqueue(petrolAmount.Peek());
-                    petrolAmount.Dequeue();
-                    distance.Enqueue(distance.Peek());
-                    distance.Dequeue();
-                    startIndex = pupmsCount - (pupmsCount - checkpointCounter);
-                }
+                else // if we don't have enough amount of fuel to reach next station
+                {     
+                    petrolAmount.Enqueue(petrolAmount.Dequeue()); // current station is put last in the queue so we start from next station
+                    distance.Enqueue(distance.Dequeue());
+                    
+                    fuel = petrolAmount.Peek(); // set initial fuel from next station
+                    stationsPassedCounter = 0; // restart counter                    
+                }                
+
+                currentIndex++;
             }
 
-            Console.WriteLine(startIndex);
-            
+            // Print output
+            Console.WriteLine(smallestIndex);
         }
     }
 }
