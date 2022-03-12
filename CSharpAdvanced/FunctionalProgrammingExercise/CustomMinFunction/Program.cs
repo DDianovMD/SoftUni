c593781ab5027f918e3d19598905d07f75a17aa4
DDianovMD <ddianovmd@abv.bg>
2022-03-12 22:18:17 +0200

    Solved CustomMinFunction problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/CustomMinFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int smallestNumber = int.MaxValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (smallestNumber > numbers[i])
+                {
+                    smallestNumber = numbers[i];
+                }
+            }
+
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
