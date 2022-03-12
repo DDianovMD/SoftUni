7c4823dea3c2bc25036f8a4edca0cc0ac1c0f09d
DDianovMD <ddianovmd@abv.bg>
2022-03-12 23:16:09 +0200

    Solved PredicateForNames problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/PredicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Length <= nameLength)
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+        }
+    }
+}
