a207057130435001d981551da89b54f3f3232837
DDianovMD <ddianovmd@abv.bg>
2022-03-12 22:49:16 +0200

    Solved AppliedArithmetics problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/AppliedArithmetics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            StringBuilder command = new StringBuilder();
+
+            while (true)
+            {
+                command.Append(Console.ReadLine());
+
+                if (command.ToString().ToLower() == "end")
+                {
+                    break;
+                }
+
+                if (command.ToString().ToLower() == "add")
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        numbers[i] += 1;
+                    }
+                }
+                else if (command.ToString().ToLower() == "multiply")
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        numbers[i] *= 2;
+                    }
+                }
+                else if (command.ToString().ToLower() == "subtract")
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        numbers[i] -= 1;
+                    }
+                }
+                else if (command.ToString().ToLower() == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+
+                command.Clear();
+            }
+        }
+    }
+}
