85e794d230ba076797e5ff85dcf1896c0acff8c9
DDianovMD <ddianovmd@abv.bg>
2022-03-12 15:53:13 +0200

    Solved CountSymbols problem

---
+++ CSharpAdvanced/SetsАndDictionariesAdvancedExercise/CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput = Console.ReadLine();
+
+            Dictionary<int, int> characterCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (characterCount.ContainsKey((int)userInput[i]) == false)
+                {
+                    characterCount.Add((int)userInput[i], 1);
+                }
+                else
+                {
+                    characterCount[(int)userInput[i]] += 1;
+                }
+            }
+
+            foreach (var charAsNum in characterCount.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{(char)charAsNum.Key}: {charAsNum.Value} time/s");
+            }
+        }
+    }
+}
