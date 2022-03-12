0b1f362ac366f8d2392e249931922137d5caf2a6
DDianovMD <ddianovmd@abv.bg>
2022-03-12 14:14:30 +0200

    Solved EvenTimes problem

---
+++ CSharpAdvanced/SetsАndDictionariesAdvancedExercise/EvenTimes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main()
+        {
+            // key - unique number
+            // value - times of occurance
+
+            int integersCount = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            StringBuilder userInput = new StringBuilder();
+
+            for (int i = 0; i < integersCount; i++)
+            {
+                userInput.Append(Console.ReadLine());
+
+                if (numbers.ContainsKey(int.Parse(userInput.ToString())) == false)
+                {
+                    numbers.Add(int.Parse(userInput.ToString()), 1);
+                }
+                else
+                {
+                    numbers[int.Parse(userInput.ToString())] += 1;
+                }
+
+                userInput.Clear();
+            }
+
+            foreach (var kvp in numbers)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+        }
+    }
+}
