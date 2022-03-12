7f1b8ac982cfbe31c42e72358f2978f6b4399be9
DDianovMD <ddianovmd@abv.bg>
2022-03-12 23:48:04 +0200

    Solved ListOfPredicates problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/ListOfPredicates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main()
+        {
+            int upperBorder = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= upperBorder; i++)
+            {
+                numbers.Add(i);
+            }
+
+            int[] predicates = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            bool isDividable = true;
+
+            List<int> filteredNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < predicates.Length; j++)
+                {
+                    if (numbers[i] % predicates[j] != 0)
+                    {
+                        isDividable = false;
+                    }
+                }
+
+                if (isDividable)
+                {
+                    filteredNumbers.Add(numbers[i]);
+                }
+
+                isDividable = true;
+            }
+
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+    }
+}
