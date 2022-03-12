ebe6a34a42d19a1cbaca5a7aeccc486d803914e9
DDianovMD <ddianovmd@abv.bg>
2022-03-12 23:05:17 +0200

    Solved ReverseAndExclude problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int numberToDivideWith = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % numberToDivideWith == 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
