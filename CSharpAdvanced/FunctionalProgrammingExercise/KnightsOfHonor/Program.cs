84dc655f0c9d56028d1212e0581433e409cec08d
DDianovMD <ddianovmd@abv.bg>
2022-03-12 21:47:55 +0200

    Solved KnightsOfHonor problem

---
+++ CSharpAdvanced/FunctionalProgrammingExercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] userInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var item in userInput)
+            {
+                Console.WriteLine($"Sir {item}");
+            }
+        }
+    }
+}
