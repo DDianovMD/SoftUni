eec44f7203aa3fda446fe548ac9ef34ea070920b
DDianovMD <ddianovmd@abv.bg>
2022-03-12 13:10:17 +0200

    Solved PeriodicTable problem

---
+++ CSharpAdvanced/SetsАndDictionariesAdvancedExercise/PeriodicTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            List<string> periodicTable = new List<string>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] currentInput = Console.ReadLine()
+                                               .Split(" ")
+                                               .ToArray();
+
+                foreach (var element in currentInput)
+                {
+                    if (periodicTable.Contains(element) == false)
+                    {
+                        periodicTable.Add(element);
+                    }
+                }
+            }
+
+            periodicTable.Sort();
+
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
