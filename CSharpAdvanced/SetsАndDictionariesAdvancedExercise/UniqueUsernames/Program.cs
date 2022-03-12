f08ad20038f0d570ebe7c0667d752ab848ab0b41
DDianovMD <ddianovmd@abv.bg>
2022-03-12 12:30:38 +0200

    Solved UniqueUsernames problem

---
+++ CSharpAdvanced/SetsАndDictionariesAdvancedExercise/UniqueUsernames/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            List<string> names = new List<string>();
+            StringBuilder userInput = new StringBuilder();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                userInput.Append(Console.ReadLine());
+
+                if (names.Contains(userInput.ToString()) == false)
+                {
+                    names.Add(userInput.ToString());
+                }
+                
+                userInput.Clear();
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
