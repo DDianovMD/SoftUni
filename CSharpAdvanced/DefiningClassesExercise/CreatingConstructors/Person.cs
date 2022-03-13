5324bb18f424108f50dbdbaea141ae65a99a2507
DDianovMD <ddianovmd@abv.bg>
2022-03-13 13:17:33 +0200

    Solved CreatingConstructors problem

---
+++ CSharpAdvanced/DefiningClassesExercise/CreatingConstructors/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+        }
+
+        public Person(int age)
+        {
+            this.age = age;
+            this.name = "No name";
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
