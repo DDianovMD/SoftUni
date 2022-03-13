55f64c55bd445a5d49d9206e0ef1508dc678e060
DDianovMD <ddianovmd@abv.bg>
2022-03-13 13:09:28 +0200

    Solved DefineAClassPerson problem

---
+++ CSharpAdvanced/DefiningClassesExercise/DefineAClassPerson/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
---
+++ CSharpAdvanced/DefiningClassesExercise/DefineAClassPerson/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+
+            Person person1 = new Person();
+            person1.Name = "George";
+            person1.Age = 18;
+
+            Person person2 = new Person();
+            person2.Name = "Jose";
+            person2.Age = 43;
+        }
+    }
+}
