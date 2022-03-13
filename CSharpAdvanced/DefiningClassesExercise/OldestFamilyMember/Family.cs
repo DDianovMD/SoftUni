29a78d20bf6effe6934b2887e09d2f7b55bc2db2
DDianovMD <ddianovmd@abv.bg>
2022-03-13 14:14:27 +0200

    Solved OldestFamilyMember problem

---
+++ CSharpAdvanced/DefiningClassesExercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {        
+        public List<Person> FamilyMembers { get; set; }
+
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        // Methods
+
+        public void AddMember(Person person)
+        {
+            FamilyMembers.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person person = FamilyMembers.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return person;
+        }
+    }
+}
---
+++ CSharpAdvanced/DefiningClassesExercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] userInput = Console.ReadLine()
+                                            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person currentPerson = new Person(userInput[0], int.Parse(userInput[1]));
+
+                family.AddMember(currentPerson);
+            }
+
+            Person oldestFamilyMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+        }
+    }
+}
