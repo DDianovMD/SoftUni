4e027e461a31d10f64740eeb19668495c1477dbb
DDianovMD <ddianovmd@abv.bg>
2022-03-15 06:32:00 +0200

    Solved DateModifier problem

---
+++ CSharpAdvanced/DefiningClassesExercise/DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static int CompareDates(string date1, string date2)
+        {
+            DateTime firstDate = DateTime.Parse(date1);
+            DateTime secondDate = DateTime.Parse(date2);
+
+            int difference = Math.Abs((firstDate - secondDate).Days);
+
+            return difference;
+        }
+    }
+}
---
+++ CSharpAdvanced/DefiningClassesExercise/DateModifier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] dates = new string[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                dates[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine(DateModifier.CompareDates(dates[0], dates[1]));
+            
+        }
+    }
+}
