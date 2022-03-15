373ddbcbdd4134033ae2012c1d701bedda8ad336
DDianovMD <ddianovmd@abv.bg>
2022-03-15 17:34:53 +0200

    Solved SpeedRacing problem

---
+++ CSharpAdvanced/DefiningClassesExercise/SpeedRacing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumption;
+            this.TravelledDistance = 0;
+        }
+
+        public void Drive(double amountOfKm)
+        {
+            if (this.FuelAmount >= amountOfKm * this.FuelConsumptionPerKilometer)
+            {
+                this.FuelAmount -= amountOfKm * this.FuelConsumptionPerKilometer;
+                this.TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/DefiningClassesExercise/SpeedRacing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            List<Car> carsList = new List<Car>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] userInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car currentCar = new Car(userInput[0], double.Parse(userInput[1]), double.Parse(userInput[2]));
+                carsList.Add(currentCar);
+            }
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0].ToLower() == "end")
+                {
+                    break;
+                }
+
+                for (int i = 0; i < carsList.Count; i++)
+                {
+                    if (carsList[i].Model == command[1])
+                    {
+                        carsList[i].Drive(double.Parse(command[2]));
+                    }
+                }
+            }
+
+            foreach (var car in carsList)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
