7bfab31af4ce51233781ed8cbbb6e50b987d6bf6
DDianovMD <ddianovmd@abv.bg>
2022-03-21 19:58:20 +0200

    Solved GenericBoxOfString problem

---
+++ CSharpAdvanced/Generics/GenericBoxOfString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                command = Console.ReadLine();
+
+                Box<string> currentBox = new Box<string>(command);
+                Console.WriteLine(currentBox.ToString());
+            }
+        }
+    }
+}
