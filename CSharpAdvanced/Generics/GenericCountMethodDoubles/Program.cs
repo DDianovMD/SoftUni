c5573d6357d08d509e381558873d08ef0d136f45
DDianovMD <ddianovmd@abv.bg>
2022-03-21 22:02:32 +0200

    Solved GenericCountMethodDoubles problem

---
+++ CSharpAdvanced/Generics/GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDoubles
+
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            List<double> inputs = new List<double>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string userInput = Console.ReadLine();
+                inputs.Add(double.Parse(userInput));
+            }
+
+            string elementToCompareWith = Console.ReadLine();
+
+            GetGreaterValueElementsCount<double>(inputs, double.Parse(elementToCompareWith));
+        }
+
+        public static void GetGreaterValueElementsCount<T>(List<T> list, T comparator) where T : IComparable
+        {
+            int counter = 0;
+
+            foreach (var item in list)
+            {
+                if (item.CompareTo(comparator) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
