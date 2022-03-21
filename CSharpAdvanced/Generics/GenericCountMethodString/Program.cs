5a92730a68d93e18701aaf5458a42f31b94dd54f
DDianovMD <ddianovmd@abv.bg>
2022-03-21 21:54:41 +0200

    Solved GenericCountMethodString problem

---
+++ CSharpAdvanced/Generics/GenericCountMethodString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            List<string> inputs = new List<string>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string userInput = Console.ReadLine();
+                inputs.Add(userInput);
+            }
+
+            string elementToCompareWith = Console.ReadLine();
+
+            GetGreaterValueElementsCount<string>(inputs, elementToCompareWith);
+        }
+
+        public static void GetGreaterValueElementsCount<T>(List<T> list, T comparator)
+        {
+            int counter = 0;
+
+            foreach (var item in list)
+            {
+                if (item.ToString().CompareTo(comparator.ToString()) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
