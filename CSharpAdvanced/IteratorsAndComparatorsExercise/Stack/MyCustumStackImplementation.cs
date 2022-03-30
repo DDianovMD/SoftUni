63f56c6f4ee44c36309c23042afea167ae6c8126
DDianovMD <ddianovmd@abv.bg>
2022-03-30 16:48:16 +0300

    Solved Stack problem

---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/Stack/MyCustumStackImplementation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class MyCustumStackImplementation<T> : IEnumerable<T>
+    {
+        private List<T> _stack;
+
+        public MyCustumStackImplementation()
+        {
+            _stack = new List<T>();
+        }
+
+        public void Push(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                _stack.Add(item);
+            }
+        }
+
+        public void Pop()
+        {
+            if (_stack.Count > 0)
+            {                
+                _stack.RemoveAt(_stack.Count - 1);
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            _stack.Reverse();
+
+            foreach (var item in _stack)
+            {
+                yield return item;
+            }
+
+            _stack.Reverse();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main()
+        {
+            MyCustumStackImplementation<string> custumStack = new MyCustumStackImplementation<string>();
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                var userInput = command.Split(", ").ToList();
+
+                // Push given items in the custum stack
+                if (userInput[0].ToLower().Contains("push"))
+                {
+                    userInput[0] = userInput[0].Substring(5);
+                    custumStack.Push(userInput);
+                }
+                // Use custum Pop() method to remove last added item in the custum stack
+                else if (userInput[0].ToLower() == "pop")
+                {
+                    custumStack.Pop();
+                }
+
+                command = Console.ReadLine();
+            }
+
+            // Print twice all elements in custum stack implementation
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in custumStack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
