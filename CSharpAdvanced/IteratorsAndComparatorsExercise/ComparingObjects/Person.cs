68a530e9268b76405917d7f19008990b0d709f64
DDianovMD <ddianovmd@abv.bg>
2022-03-30 21:50:54 +0300

    Solved ComparingObjects problem

---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/ComparingObjects/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public string Name { get => this.name; private set => this.name = value; }
+        public int Age { get => this.age; private set => this.age = value; }
+        public string Town { get => this.town; private set => this.town = value; }
+
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+
+                if (result == 0)
+                {
+                    result = this.Town.CompareTo(other.Town);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/ComparingObjects/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0].ToLower() != "end")
+            {
+                Person currentPerson = new Person(command[0], int.Parse(command[1]), command[2]);
+                people.Add(currentPerson);
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            int personPosition = int.Parse(Console.ReadLine());
+
+            int matchesCounter = 0;
+            int nonMatchesCounter = 0;
+            int totalNumberOfPeople = people.Count;
+
+            for (int i = 0; i < totalNumberOfPeople; i++)
+            {
+
+                int result = people[i].CompareTo(people[personPosition - 1]);
+
+                if (result == 0)
+                {
+                    matchesCounter++;
+                }
+                else
+                {
+                    nonMatchesCounter++;
+                }
+
+            }
+
+            if (matchesCounter >= 2)
+            {
+                Console.WriteLine($"{matchesCounter} {nonMatchesCounter} {totalNumberOfPeople}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
