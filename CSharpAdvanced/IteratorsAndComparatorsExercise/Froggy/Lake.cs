b542ae58587d5e6ad45a4ed1376014207ce7e2d2
DDianovMD <ddianovmd@abv.bg>
2022-03-30 17:31:30 +0300

    Solved Froggy problem

---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/Froggy/Lake.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        private List<T> _stones;
+        private List<T> evenStones;
+        private List<T> oddStones;
+        public Lake(IEnumerable<T> stones)
+        {
+            _stones = new List<T>(stones);
+            evenStones = new List<T>();
+            oddStones = new List<T>();
+
+            for (int i = 0; i < _stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenStones.Add(_stones[i]);
+                }
+                else
+                {
+                    oddStones.Add(_stones[i]);
+                }
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in evenStones)
+            {
+                yield return item;
+            }
+
+            oddStones.Reverse();
+
+            foreach (var item in oddStones)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var stones = Console.ReadLine().Split(", ").Select(int.Parse);
+
+            Lake<int> lake = new Lake<int>(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
