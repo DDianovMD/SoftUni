da1d1ac05b4a6256fef27cf1f2ca2a77a93055a2
DDianovMD <ddianovmd@abv.bg>
2022-03-30 14:17:12 +0300

    Solved ListyIterator problem

---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> _collection;
+        private int _index = 0;
+        public ListyIterator(IEnumerable<T> collection)
+        {
+            _collection = new List<T>(collection);
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                _index++;
+                return true;
+            }
+
+            return false;
+        }
+        public bool HasNext()
+        {
+            if (_index + 1 < _collection.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (_collection.Count > 0)
+            {
+                Console.WriteLine(_collection[_index]);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/IteratorsAndComparatorsExercise/ListyIterator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            var collection = Console.ReadLine()
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                    .ToList();
+           
+            collection.RemoveAt(0);
+
+            ListyIterator<string> iterator = new ListyIterator<string>(collection);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command.ToLower() == "move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+                else if (command.ToLower() == "print")
+                {                    
+                    try
+                    {
+                        iterator.Print();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("Invalid Operation!");
+                    }                    
+                }
+                else if (command.ToLower() == "hasnext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
