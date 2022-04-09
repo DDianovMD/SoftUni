a7b10a2cbb6dc731dce8661191c3f0ac6c457c49
DDianovMD <ddianovmd@abv.bg>
2022-04-09 09:46:51 +0300

    Solved PrimaryDiagonal problem

---
+++ CSharpAdvanced/MultidimensionalArraysLab/PrimaryDiagonal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Receive matrix's rows and cols
+
+            int rowsAndCols = int.Parse(Console.ReadLine());
+                                
+
+            // Create matrix
+
+            int[,] matrix = new int[rowsAndCols, rowsAndCols];
+
+            // Fill matrix
+
+            for (int rows = 0; rows < rowsAndCols; rows++)
+            {
+                int[] currentDimension = Console.ReadLine()
+                                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(int.Parse)
+                                                .ToArray();
+
+                for (int col = 0; col < rowsAndCols; col++)
+                {
+                    matrix[rows, col] = currentDimension[col];
+                }
+            }
+
+            // Calculate primary diagonal
+
+            int primaryDiagonalSum = 0;
+
+            for (int row = 0; row < rowsAndCols; row++)
+            {
+                primaryDiagonalSum += matrix[row, row];
+            }
+
+            // Print result
+
+            Console.WriteLine(primaryDiagonalSum);
+        }
+    }
+}
