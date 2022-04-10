e9bb93ce2a28dafc133df085f47aa6c8c2cae6c7
DDianovMD <ddianovmd@abv.bg>
2022-04-10 11:51:00 +0300

    Solved Largest3Numbers problem

---
+++ CSharpAdvanced/SetsAndDictionariesAdvancedLab/Largest3Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            numbers = numbers.OrderByDescending(x => x).ToArray();
+
+            if (numbers.Length >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+        }
+    }
+}
