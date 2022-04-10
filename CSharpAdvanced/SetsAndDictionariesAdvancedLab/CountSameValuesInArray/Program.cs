e031ccb7ae95589012cb0788e5f3bcf755f69988
DDianovMD <ddianovmd@abv.bg>
2022-04-10 11:24:08 +0300

    Solved CountSameValuesInArray problem

---
+++ CSharpAdvanced/SetsAndDictionariesAdvancedLab/CountSameValuesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<double, int> valuesCounter = new Dictionary<double, int>();
+
+            double[] values = Console.ReadLine()
+                                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(double.Parse)
+                                     .ToArray();
+
+            foreach (var value in values)
+            {
+                if (valuesCounter.ContainsKey(value) == false)
+                {
+                    valuesCounter.Add(value, 1);
+                }
+                else
+                {
+                    valuesCounter[value]++;
+                }
+            }
+
+            foreach (var kvp in valuesCounter)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
