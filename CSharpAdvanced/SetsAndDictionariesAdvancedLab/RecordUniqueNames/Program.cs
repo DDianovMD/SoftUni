32773682321e024bf60e2f904ea3e7c0017df0c3
DDianovMD <ddianovmd@abv.bg>
2022-04-10 13:37:27 +0300

    Solved RecordUniqueNames problem

---
+++ CSharpAdvanced/SetsAndDictionariesAdvancedLab/RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
