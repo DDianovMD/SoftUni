850be681a7a03f0a84764e51f33d88405102c261
DDianovMD <ddianovmd@abv.bg>
2022-04-22 17:22:23 +0300

    Solved CountUppercaseWords problem

---
+++ CSharpAdvanced/FunctionalProgrammingLab/CountUppercaseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .Where(x => char.IsUpper(x[0]))
+                   .ToList()
+                   .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
