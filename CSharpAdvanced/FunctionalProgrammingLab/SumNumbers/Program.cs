b8dea3ac455ebe87f69c6713635a85e99d921b2f
DDianovMD <ddianovmd@abv.bg>
2022-04-22 17:08:59 +0300

    Solved SumNumbers problem

---
+++ CSharpAdvanced/FunctionalProgrammingLab/SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] userInput = Console.ReadLine()
+                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+            Console.WriteLine(userInput.Length);
+            Console.WriteLine(userInput.Sum());
+        }
+    }
+}
