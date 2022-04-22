f4b68946783a056c0ee73a8aaa01b56d40526f20
DDianovMD <ddianovmd@abv.bg>
2022-04-22 16:38:39 +0300

    Solved SortEvenNumbers problem

---
+++ CSharpAdvanced/FunctionalProgrammingLab/SortEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] userInput = Console.ReadLine()
+                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .Where(x => x % 2 == 0)
+                                     .OrderBy(x => x)
+                                     .ToArray();
+
+            Console.WriteLine(string.Join(", ", userInput));
+        }
+    }
+}
