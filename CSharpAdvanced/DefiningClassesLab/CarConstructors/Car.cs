ef591840beb9e51e63ebcc342e8f56bbde29a9ce
DDianovMD <ddianovmd@abv.bg>
2022-04-24 12:05:38 +0300

    Solved CarConstructors problem

---
+++ CSharpAdvanced/DefiningClassesLab/CarConstructors/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Car
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get => this.make; set => this.make = value; }
+        public string Model { get => this.model; set => this.model = value; }
+        public int Year { get => this.year; set => this.year = value; }
+        public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+        public double FuelConsumption { get => fuelConsumption; set => fuelConsumption = value; }
+
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.fuelQuantity - this.fuelConsumption * distance >= 0)
+            {
+                this.fuelQuantity = this.fuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string make = this.make;
+            string model = this.model;
+            string year = this.year.ToString();
+            string fuel = $"{this.fuelQuantity:F2}";
+
+            return  make + Environment.NewLine +
+                    model + Environment.NewLine + 
+                    year + Environment.NewLine + 
+                    fuel;
+        }
+    }
+}
