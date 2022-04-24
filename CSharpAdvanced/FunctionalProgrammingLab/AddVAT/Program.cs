81fbc8e5f3a117335a4f420c8f9b5c7cde298ca2
DDianovMD <ddianovmd@abv.bg>
2022-04-24 09:31:00 +0300

    Solved AddVAT problem

---
+++ CSharpAdvanced/FunctionalProgrammingLab/AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split(", ")
+                .Select(x => decimal.Parse(x) * (decimal)1.2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:F2}"));
+        }
+    }
+}
