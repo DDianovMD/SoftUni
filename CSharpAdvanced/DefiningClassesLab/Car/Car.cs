82f6e3ebea899433d8f35fd6f039998b1cec1bb1
DDianovMD <ddianovmd@abv.bg>
2022-04-24 11:50:02 +0300

    Solved Car problem

---
+++ CSharpAdvanced/DefiningClassesLab/Car/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace Car
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get => this.make; set => this.make = value; }
+        public string Model { get => this.model; set => this.model = value; }
+        public int Year { get => this.year; set => this.year = value; }
+    }
+}
