776f5e17c2306df3c57860e0e5f7ad42f27eb5e5
DDianovMD <ddianovmd@abv.bg>
2022-05-03 00:47:27 +0300

    Solved BoxOfT problem

---
+++ CSharpAdvanced/GenericsLab/Box/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> internalList { get; set; } = new List<T>();
+        public int Count { get => internalList.Count; }
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+
+            return element;
+        }
+    }
+}
