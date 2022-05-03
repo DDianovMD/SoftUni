ea3b1b1279db382038a57921fb2128be29a7f66a
DDianovMD <ddianovmd@abv.bg>
2022-05-03 13:21:27 +0300

    Solved GenericArrayCreator problem

---
+++ CSharpAdvanced/GenericsLab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            /*
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+            */
+        }
+    }
+}
