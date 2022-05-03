0e05ecd5cd63fa9206b54880baa55bd7964a4f78
DDianovMD <ddianovmd@abv.bg>
2022-05-03 13:31:10 +0300

    Solved GenericScale problem

---
+++ CSharpAdvanced/GenericsLab/GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public T Left { get => this.left; set => this.left = value; }
+        public T Right { get => this.right; set => this.right = value; }
+
+        public bool AreEqual()
+        {
+            return this.left.Equals(this.right);
+        }
+        
+    }
+}
