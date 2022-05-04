baec81549d0a537bb90c5067e4af369f69a69d12
DDianovMD <ddianovmd@abv.bg>
2022-05-05 00:55:15 +0300

    Solved SingleInheritance problem

---
+++ CSharpOOP/InheritanceLab/SingleInheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            /*
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+            */
+        }
+    }
+}
