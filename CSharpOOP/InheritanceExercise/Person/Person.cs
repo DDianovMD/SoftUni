a566ca339369ca988100e27db7e270d1efcfd4ca
DDianovMD <ddianovmd@abv.bg>
2022-05-05 11:15:07 +0300

    Solved Person problem

---
+++ CSharpOOP/InheritanceExercise/Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
