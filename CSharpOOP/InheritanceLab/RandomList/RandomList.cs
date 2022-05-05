6a896e89218d98a0921f4c97e0a7fcd273e527f4
DDianovMD <ddianovmd@abv.bg>
2022-05-05 09:43:12 +0300

    Solved RandomList problem

---
+++ CSharpOOP/InheritanceLab/RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random randomInt = new Random();
+
+            // Generating random index inside the bounds of the list
+            int index = randomInt.Next(0, this.Count - 1);
+
+            // Save element on that index and remove it from the list
+            string randomString = this[index];
+            this.RemoveAt(index);
+
+            // Return removed element
+            return randomString;
+        }
+    }
+}
