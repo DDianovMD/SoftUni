8b3a9990af7c2ad01df2bef9b4baa423d6ea2fe0
DDianovMD <ddianovmd@abv.bg>
2022-05-22 22:29:30 +0300

    Solved Persons problem

---
+++ CSharpOOP/EncapsulationLab/Persons/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] userInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person currentPerson = new Person(userInput[0], userInput[1], int.Parse(userInput[2]));
+
+                people.Add(currentPerson);
+            }
+
+            foreach (Person person in people.OrderBy(x => x.FirstName).ThenBy(x => x.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
