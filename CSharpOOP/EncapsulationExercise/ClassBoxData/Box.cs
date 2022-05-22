2f7bdba7ad2624129618d137fb00833c208d9dd1
DDianovMD <ddianovmd@abv.bg>
2022-05-23 02:03:28 +0300

    Solved ClassBoxData problem

---
+++ CSharpOOP/EncapsulationExercise/ClassBoxData/Box.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private readonly double length;
+        private readonly double width;
+        private readonly double height;
+
+        public Box(double length, double width, double height)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentException("Length cannot be zero or negative.");
+            }
+            else
+            {
+                this.length = length;
+            }
+
+            if (width <= 0)
+            {
+                throw new ArgumentException("Width cannot be zero or negative.");
+            }
+            else
+            {
+                this.width = width;
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Height cannot be zero or negative.");
+            }
+            else
+            {
+                this.height = height;
+            }
+        }
+
+        public double Length => length;
+        public double Width => width;
+        public double Height => height;
+
+        public double SurfaceArea()
+        {
+            //Surface Area = 2lw + 2lh + 2wh
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            //Lateral Surface Area = 2lh + 2wh
+            return 2 * length * height + 2 * width * height;
+        }
+
+        public double Volume()
+        {
+            //Volume = lwh
+            return length * width * height;
+        }
+    }
+}
---
+++ CSharpOOP/EncapsulationExercise/ClassBoxData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
