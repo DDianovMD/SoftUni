82d373acbf79c8140f3a399d98eea81fb3bed548
DDianovMD <ddianovmd@abv.bg>
2022-05-22 23:06:26 +0300

    Solved Validation problem by refactoring Salary problem code

--- CSharpOOP/EncapsulationLab/Persons/Person.cs
+++ CSharpOOP/EncapsulationLab/Persons/Person.cs
@@ -1,7 +1,14 @@
-﻿namespace PersonsInfo
+﻿using System;
+
+namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
         public Person(string firstName, string lastName, int age)
         {
             FirstName = firstName;
@@ -17,10 +24,82 @@
             Salary = salary;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-        public decimal Salary { get; private set; }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            private set
+            {
+                if (value.Length >= 3)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            private set
+            {
+                if (value.Length >= 3)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            private set
+            {
+                if (value > 1)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+
+            private set
+            {
+                if (value > 650m)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
