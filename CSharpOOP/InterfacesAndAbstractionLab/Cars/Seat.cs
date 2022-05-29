7e4e3fedc7341d68836d0ad9765dbac4050240be
DDianovMD <ddianovmd@abv.bg>
2022-05-29 19:58:18 +0300

    Solved Cars problem

---
+++ CSharpOOP/InterfacesAndAbstractionLab/Cars/Seat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+
+        public string Start()
+        {
+            return $"Engine start";
+        }
+
+        public string Stop()
+        {
+            return $"Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model}"
+                    + Environment.NewLine + this.Start() 
+                    + Environment.NewLine + this.Stop();
+        }
+    }
+}
---
+++ CSharpOOP/InterfacesAndAbstractionLab/Cars/Tesla.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+        public int Battery { get => battery; set => battery = value; }
+
+        public string Start()
+        {
+            return $"Engine start";
+        }
+
+        public string Stop()
+        {
+            return $"Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model} with {Battery} Batteries"
+                    + Environment.NewLine + this.Start()
+                    + Environment.NewLine + this.Stop();
+        }
+    }
+}
