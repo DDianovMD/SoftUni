1ce834e911a17a49e43ae91616bdcf6f4495165b
DDianovMD <ddianovmd@abv.bg>
2022-05-29 19:27:23 +0300

    Solved Shapes problem

---
+++ CSharpOOP/InterfacesAndAbstractionLab/Shapes/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', width));
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.WriteLine('*' + new string(' ', width - 2) + '*');
+            }
+            Console.WriteLine(new string('*', width));
+        }
+    }
+}
