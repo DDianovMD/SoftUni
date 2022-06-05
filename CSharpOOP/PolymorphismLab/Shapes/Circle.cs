09f2191a5d3683083b9e7d32ce752985f42e42e9
DDianovMD <ddianovmd@abv.bg>
2022-06-05 22:39:27 +0300

    Solved Shapes problem

---
+++ CSharpOOP/PolymorphismLab/Shapes/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius { get => radius; set => radius = value; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            double rIn = Radius - 0.4;
+            double rOut = Radius + 0.4;
+
+            for (double y = Radius; y >= -Radius; --y)
+            {
+                for (double x = -Radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        sb.Append("*");
+                    }
+                    else
+                    {
+                        sb.Append(" ");
+                    }
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharpOOP/PolymorphismLab/Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get => height; set => height = value; }
+        public int Width { get => width; set => width = value; }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Height + 2 * Width;
+        }
+
+        public override string Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(new string('*', Width));
+            for (int i = 0; i < Height - 2; i++)
+            {
+                sb.AppendLine('*' + new string(' ', Width - 2) + '*');
+            }
+            sb.AppendLine(new string('*', Width));
+
+            return sb.ToString();
+        }
+    }
+}
