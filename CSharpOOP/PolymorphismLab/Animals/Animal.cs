371f77c3a2b6c780cf4aa053539e45920f9d9521
DDianovMD <ddianovmd@abv.bg>
2022-06-05 19:41:25 +0300

    Solved Animals problem

---
+++ CSharpOOP/PolymorphismLab/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        protected string name;
+        protected string favouriteFood;
+
+        public Animal(string name, string favFood)
+        {
+            this.name = name;
+            favouriteFood = favFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favouriteFood}";
+        }
+    }
+}
---
+++ CSharpOOP/PolymorphismLab/Animals/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favFood)
+            : base(name, favFood) { }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "DJAAF";
+        }
+    }
+}
