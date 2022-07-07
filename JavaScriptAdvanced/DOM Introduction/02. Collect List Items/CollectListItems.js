69a4119444f36681bc7003146a773ce2c19bf6c5
DDianovMD <ddianovmd@abv.bg>
2022-07-08 01:21:51 +0300

    Solved CollectListItems problem

---
+++ JavaScriptAdvanced/DOM Introduction/02. Collect List Items/CollectListItems.js
@@ -0,0 +1,6 @@
+function extractText() {
+
+    let ulElement = document.getElementById('items');
+    let textAreaElement = document.getElementById('result');
+    textAreaElement.textContent = ulElement.innerText;
+}
