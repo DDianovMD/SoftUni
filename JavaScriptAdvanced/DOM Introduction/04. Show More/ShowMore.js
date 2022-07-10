c946b04692f8aea0451a1bb376a2fa17d4c247fc
DDianovMD <ddianovmd@abv.bg>
2022-07-10 10:38:21 +0300

    Solved ShowMore problem

---
+++ JavaScriptAdvanced/DOM Introduction/04. Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let readMoreLinkElement = document.getElementById('more');
+    let hiddenTextElement = document.getElementById('text');
+
+    readMoreLinkElement.style = 'display: none';
+    hiddenTextElement.style = 'display: inline';
+}
