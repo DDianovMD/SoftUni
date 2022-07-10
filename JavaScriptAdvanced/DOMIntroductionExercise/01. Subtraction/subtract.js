7913fe6b9c833fb6a9dd916cf30e499bacee2d4b
Daniel Dianov, M.D <87268073+DDianovMD@users.noreply.github.com>
2022-07-10 16:28:17 +0300

    Delete JavaScriptAdvanced/DOMIntroductionExercise directory
    wrong commit

--- JavaScriptAdvanced/DOMIntroductionExercise/01. Subtraction/subtract.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-    let firstNumberElement = document.getElementById('firstNumber');
-    let secondNumberElement = document.getElementById('secondNumber');
-    let resultElement = document.getElementById('result');
-
-    resultElement.innerText = Number(firstNumberElement.value) - Number(secondNumberElement.value);
-}
