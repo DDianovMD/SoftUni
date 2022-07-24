918a0be5aa5de9c70231698aae48f2434e820c86
DDianovMD <ddianovmd@abv.bg>
2022-07-24 10:51:55 +0300

    Solved DatabaseFirst problem

---
+++ CSharpDatabasesAdvanced/EntityFrameworkIntroductionExercise/DatabaseFirst/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
