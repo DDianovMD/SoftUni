9227eae74c772cecde42cbb50c4a837c7ceddebe
DDianovMD <ddianovmd@abv.bg>
2022-07-25 17:10:58 +0300

    Solved EmployeesWithSalaryOver50000 problem

--- CSharpDatabasesAdvanced/EntityFrameworkIntroductionExercise/DatabaseFirst/StartUp.cs
+++ CSharpDatabasesAdvanced/EntityFrameworkIntroductionExercise/DatabaseFirst/StartUp.cs
@@ -12,29 +12,28 @@ namespace SoftUni
         {
             using (var db = new SoftUniContext())
             {
-                Console.WriteLine(GetEmployeesFullInformation(db));
+                Console.WriteLine(GetEmployeesWithSalaryOver50000(db));
             }
         }
 
-        public static string GetEmployeesFullInformation(SoftUniContext context)
+        //                                 04. Employees with Salary Over 50 000
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
             var sb = new StringBuilder();
 
             var result = context.Employees
-                .OrderBy(x => x.EmployeeId)
-                .Select(x => new
-                {
-                    firstName = x.FirstName,
-                    lastName = x.LastName,
-                    middleName = x.MiddleName,
-                    jobTitle = x.JobTitle,
-                    salary = x.Salary
-                })
-                .ToList();
+               .Where(x => x.Salary > 50000)
+               .OrderBy(x => x.FirstName)
+               .Select(x => new
+               {
+                   firstName = x.FirstName,
+                   salary = x.Salary
+               })
+               .ToList();
 
             foreach (var employee in result)
             {
-                sb.Append($"{employee.firstName} {employee.lastName} {employee.middleName} {employee.jobTitle} {employee.salary:F2}")
+                sb.Append($"{employee.firstName} - {employee.salary:F2}")
                   .Append(Environment.NewLine);
             }
 
@@ -42,3 +41,30 @@ namespace SoftUni
         }
     }
 }
+
+//                                         03. Employees Full Information solution
+
+//  public static string GetEmployeesFullInformation(SoftUniContext context)
+//  {
+//      var sb = new StringBuilder();
+
+//      var result = context.Employees
+//         .OrderBy(x => x.EmployeeId)
+//         .Select(x => new
+//          {
+//              firstName = x.FirstName,
+//              lastName = x.LastName,
+//              middleName = x.MiddleName,
+//              jobTitle = x.JobTitle,
+//              salary = x.Salary
+//          })
+//          .ToList();
+
+//      foreach (var employee in result)
+//      {
+//          sb.Append($"{employee.firstName} {employee.lastName} {employee.middleName} {employee.jobTitle} {employee.salary:F2}")
+//            .Append(Environment.NewLine);
+//      }
+
+//      return sb.ToString().Trim();
+//  }
