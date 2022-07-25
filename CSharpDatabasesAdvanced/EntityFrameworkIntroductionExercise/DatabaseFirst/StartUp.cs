3f7c0709738ece012013de0463c4632442b19053
DDianovMD <ddianovmd@abv.bg>
2022-07-25 16:18:34 +0300

    Solved EmployeesFullInformation problem

--- CSharpDatabasesAdvanced/EntityFrameworkIntroductionExercise/DatabaseFirst/StartUp.cs
+++ CSharpDatabasesAdvanced/EntityFrameworkIntroductionExercise/DatabaseFirst/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using SoftUni.Data;
 using SoftUni.Models;
 
@@ -8,7 +10,35 @@ namespace SoftUni
     {
         public static void Main()
         {
+            using (var db = new SoftUniContext())
+            {
+                Console.WriteLine(GetEmployeesFullInformation(db));
+            }
+        }
 
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var result = context.Employees
+                .OrderBy(x => x.EmployeeId)
+                .Select(x => new
+                {
+                    firstName = x.FirstName,
+                    lastName = x.LastName,
+                    middleName = x.MiddleName,
+                    jobTitle = x.JobTitle,
+                    salary = x.Salary
+                })
+                .ToList();
+
+            foreach (var employee in result)
+            {
+                sb.Append($"{employee.firstName} {employee.lastName} {employee.middleName} {employee.jobTitle} {employee.salary:F2}")
+                  .Append(Environment.NewLine);
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
