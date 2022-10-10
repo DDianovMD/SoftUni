9cb522db44af9910d8449ca879d00013e1a93e50
DDianovMD <ddianovmd@abv.bg>
2022-10-10 12:33:07 +0300

    Solved Renovators problem

---
+++ CSharpAdvanced/AdvancedExams/25.06.2022/Renovators/Catalog.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        private List<Renovator> renovators = new List<Renovator>();
+
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+
+        public int Count { get => this.renovators.Count; }
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+        }
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (this.Count >= this.NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (renovator.Name == null || renovator.Name == string.Empty || renovator.Type == null || renovator.Type == string.Empty)
+            {
+               return "Invalid renovator's information.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+
+            this.renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            bool result = this.renovators.Any(renovator => renovator.Name == name);
+
+            if (result)
+            {
+                this.renovators.RemoveAll(renovator => renovator.Name == name);
+            }
+
+            return result;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            return this.renovators.RemoveAll(renovator => renovator.Type == type);
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            if (this.renovators.Any(renovator => renovator.Name == name))
+            {
+                for (int i = 0; i < this.renovators.Count; i++)
+                {
+                    if (this.renovators[i].Name == name)
+                    {
+                        this.renovators[i].Hired = true;
+                        break;
+                    }
+                }
+
+                return this.renovators.Where(renovator => renovator.Name == name).ElementAt(0);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> result = new List<Renovator>();
+
+            foreach (var renovator in this.renovators.Where(renovator => renovator.Days >= days))
+            {
+                result.Add(renovator);
+            }
+
+            return result;
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Renovators available for Project {this.Project}:");
+            foreach (var renovator in this.renovators.Where(renovator => renovator.Hired == false))
+            {
+                stringBuilder.AppendLine(renovator.ToString());
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ CSharpAdvanced/AdvancedExams/25.06.2022/Renovators/Renovator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        private string name;
+        private string type;
+        private double rate;
+        private int days;
+        private bool hired = false;
+
+        public string Name { get => this.name; private set => this.name = value; }
+        public string Type { get => this.type; private set => this.type = value; }
+        public double Rate { get => this.rate; private set => this.rate = value; }
+        public bool Hired { get => this.hired; set => this.hired = value; }
+        public int Days { get => this.days; set => this.days = value; }
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.name = name;
+            this.type = type;
+            this.rate = rate;
+            this.days = days;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"-Renovator: {this.name}");
+            stringBuilder.AppendLine($"--Specialty: {this.type}");
+            stringBuilder.AppendLine($"--Rate per day: {this.rate} BGN");
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
