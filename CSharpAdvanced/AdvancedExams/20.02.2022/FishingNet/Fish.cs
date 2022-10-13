ee8d5e29b91800fcc99b430226ea0ab473f39f41
DDianovMD <ddianovmd@abv.bg>
2022-10-13 18:14:26 +0300

    Solved FishingNet problem

---
+++ CSharpAdvanced/AdvancedExams/20.02.2022/FishingNet/Fish.cs
@@ -0,0 +1,25 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+        private string fishType;
+        private double length;
+        private double weight;
+
+        public Fish(string fishType, double length, double weight)
+        {
+            FishType = fishType;
+            Length = length;
+            Weight = weight;
+        }
+
+        public string FishType { get => this.fishType; set => this.fishType = value; }
+        public double Length { get => this.length; set => this.length = value; }
+        public double Weight { get => this.weight; set => this.weight = value; }
+
+        public override string ToString()
+        {
+            return $"There is a {this.FishType}, {this.Length} cm. long, and {this.Weight} gr. in weight.";
+        }
+    }
+}
---
+++ CSharpAdvanced/AdvancedExams/20.02.2022/FishingNet/Net.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        public List<Fish> Fish = new List<Fish>();
+        private string material;
+        private int capacity;
+
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+        }
+
+        public string Material { get => this.material; set => this.material = value; }
+        public int Capacity { get => this.capacity; set => this.capacity = value; }
+        public int Count { get => this.Fish.Count; }
+
+        public string AddFish(Fish fish)
+        {
+            if (this.Count < this.Capacity)
+            {
+                if (fish.FishType != null && fish.FishType != " " && fish.Length > 0 && fish.Weight > 0)
+                {
+                    this.Fish.Add(fish);
+                    return $"Successfully added {fish.FishType} to the fishing net.";
+                }
+                else
+                {
+                    return "Invalid fish.";
+                }
+            }
+            else
+            {
+                return "Fishing net is full.";
+            }
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            int removedFishCount = this.Fish.RemoveAll(fish => fish.Weight == weight);
+
+            return removedFishCount > 0;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            return this.Fish.Where(fish => fish.FishType == fishType).ElementAt(0);
+        }
+
+        public Fish GetBiggestFish()
+        {
+            return this.Fish.OrderByDescending(fish => fish.Length).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Into the {this.Material}:");
+            foreach (Fish fish in this.Fish.OrderByDescending(fish => fish.Length))
+            {
+                result.AppendLine(fish.ToString());
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
